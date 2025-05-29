238e9dd24f0e80a65c137d8efce38076a6062faa
Daniel Moffett <moffd234@gmail.com>
2025-05-29 18:34:50 -0400

    Created placeholder functions for login and signup

--- Application/View/EntryFrame.py
+++ Application/View/EntryFrame.py
@@ -9,8 +9,14 @@ class EntryFrame(BaseFrame):
         super().__init__(parent, controller)
         self.controller: tkinter.Tk = controller  # MainWindow
 
-        self.login_button: ttk.Button = ttk.Button(self, text="Login")
-        self.signup_button: ttk.Button = ttk.Button(self, text="Sign Up")
+        self.login_button: ttk.Button = ttk.Button(self, text="Login", command=self.login)
+        self.signup_button: ttk.Button = ttk.Button(self, text="Sign Up", command=self.signup)
 
         self.login_button.grid(row=1, column=0, padx=20, pady=20, sticky="e")
         self.signup_button.grid(row=1, column=1, padx=20, pady=20, sticky="w")
+
+    def login(self) -> None:
+        pass
+
+    def signup(self) -> None:
+        pass
