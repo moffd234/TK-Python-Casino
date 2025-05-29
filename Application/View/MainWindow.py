369ed88a1a028e116b606177b2f6cc82f1cad4e6
Daniel Moffett <moffd234@gmail.com>
2025-05-29 18:19:13 -0400

    Wrote render_frame

--- Application/View/MainWindow.py
+++ Application/View/MainWindow.py
@@ -11,3 +11,14 @@ class MainWindow(tkinter.Tk):
         self.container: ttk.Frame = ttk.Frame(self)
         self.container.pack(fill="both", expand=True)
 
+    def render_frame(self, new_frame) -> None:
+        """
+        Destroys previous frame and renders new frame.
+        :param new_frame: A new Frame object.
+        :return: None
+        """
+        for frame in self.container.winfo_children():
+            frame.destroy()
+
+        frame: ttk.Frame = new_frame(self.container, self)
+        frame.pack(fill="both", expand=True)
