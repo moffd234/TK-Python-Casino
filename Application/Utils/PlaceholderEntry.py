ecaa71b23fbd3f483b63e891749da38c104cb427
Daniel Moffett <moffd234@gmail.com>
2025-05-31 05:02:59 -0400

    Refactored to Tkinter.Entry instead of ttk.Entry

--- Application/Utils/PlaceholderEntry.py
+++ Application/Utils/PlaceholderEntry.py
@@ -1,8 +1,13 @@
-from tkinter import ttk
+import tkinter
 
 
-class PlaceholderEntry(ttk.Entry):
+class PlaceholderEntry(tkinter.Entry):
     def __init__(self, master=None, placeholder="", color='grey'):
         super().__init__(master)
-        self.placeholder = placeholder
-        self.color = color
+        self.placeholder: str = placeholder
+        self.color: str = color
+        self.default_fg_color: str = self.cget('foreground')
+
+        # Bind functions on FocusIn and FocusOut
+        self.bind("<FocusIn>", self.focus_in)
+        self.bind("<FocusOut>", self.focus_out)
