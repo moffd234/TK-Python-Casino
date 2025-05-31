2eb03c33344f7175fcc5d5c830bb0c2a193ae01b
Daniel Moffett <moffd234@gmail.com>
2025-05-31 05:51:39 -0400

    Added buttons and error_label

--- Application/View/LoginFrame.py
+++ Application/View/LoginFrame.py
@@ -10,9 +10,24 @@ if TYPE_CHECKING:
 
 
 class LoginFrame(BaseFrame):
-    def __init__(self, parent: ttk.Frame, controller: 'MainWindow', login_controller: AccountController) -> None:
+    def __init__(self, parent: ttk.Frame, controller: 'MainWindow', account_controller: AccountController) -> None:
         super().__init__(parent, controller)
         self.controller = controller
-        self.login_controller = login_controller
+        self.account_controller = account_controller
 
-        self.username_entry: pEntry = pEntry(placeholder="Username", width=50)
+        self.username_entry: pEntry = pEntry(self, placeholder="Username", width=50)
+        self.password_entry: pEntry = pEntry(self, placeholder="Password", width=50)
+
+        self.login_button: ttk.Button = ttk.Button(self, text="Login", command="")
+        self.back_button: ttk.Button = ttk.Button(self, text="Back", command="")
+
+        self.error_label: ttk.Label = ttk.Label(text="Incorrect username or password")  # Hidden until there is an error
+
+        self.place_elements()
+
+    def place_elements(self):
+        self.username_entry.place(relx=0.5, rely=0.35, anchor="center")
+        self.password_entry.place(relx=0.5, rely=0.45, anchor="center")
+
+        self.login_button.place(relx=0.45, rely=0.6, anchor="center")
+        self.back_button.place(relx=0.55, rely=0.6, anchor="center")
