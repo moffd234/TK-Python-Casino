e5a00725281e739b0d718b73950c14566270e4e3
Daniel Moffett <moffd234@gmail.com>
2025-05-31 05:32:16 -0400

    Wrote tests for login

--- Tests/Controllers/test_AccountController.py
+++ Tests/Controllers/test_AccountController.py
@@ -14,3 +14,17 @@ class TestAccountController(BaseTest):
         self.account_controller = AccountController(manager)
         test_account: UserAccount = UserAccount("test_username", "ValidPassword123!", 10.0,
                                                 "email@testdomain.com", TEST_QUESTIONS)
+
+    @patch(f"{ACCOUNT_MANAGER_CLASS_PATH}.get_account",
+           return_value=UserAccount("test_username","ValidPassword123!",
+                                    50.0, "test@email.com", TEST_QUESTIONS))
+    def test_login_success(self, mock_get_account):
+        actual: bool = self.account_controller.login("test_username", "ValidPassword123!")
+
+        self.assertTrue(actual)
+        self.assert_account_info(self.account_controller.account)
+        mock_get_account.assert_called_once_with("test_username", "ValidPassword123!")
+
+    def test_login_failure(self):
+        actual: bool = self.account_controller.login("test_username", "InvalidPassword123!")
+        self.assertFalse(actual)
