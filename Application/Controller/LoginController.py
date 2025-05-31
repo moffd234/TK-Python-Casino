45948a76000f74fbf3601998535f95650e3b5188
Daniel Moffett <moffd234@gmail.com>
2025-05-31 04:07:40 -0400

    Wrote login for LoginController.py

--- Application/Controller/LoginController.py
+++ Application/Controller/LoginController.py
@@ -1,6 +1,20 @@
 from Application.Model.Accounts.AccountManager import AccountManager
+from Application.Model.Accounts.UserAccount import UserAccount
 
 
 class LoginController:
     def __init__(self, manager: AccountManager):
-        self.manager = manager
+        self.manager: AccountManager = manager
+        self.account: UserAccount | None = None
+
+    def login(self, username: str, password: str) -> UserAccount | None:
+        """
+        Attempts to authenticate a user with the given credentials
+
+        :param username: Username entered by the user
+        :param password: Password entered by the user
+        :return: UserAccount if a user was found, None otherwise
+        """
+        self.account = self.manager.get_account(username, password)
+        return self.account
+
