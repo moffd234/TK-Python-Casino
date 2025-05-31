fba5269a29e7a0b2ce25f2007b7436ed487fcac5
Daniel Moffett <moffd234@gmail.com>
2025-05-31 04:13:51 -0400

    Completed docstring for login

--- Application/Controller/AccountController.py
+++ Application/Controller/AccountController.py
@@ -4,12 +4,17 @@ from Application.Model.Accounts.UserAccount import UserAccount
 
 class AccountController:
     def __init__(self, manager: AccountManager):
+
         self.manager: AccountManager = manager
         self.account: UserAccount | None = None
 
     def login(self, username: str, password: str) -> UserAccount | None:
         """
         Attempts to authenticate a user with the given credentials
+
+        If user account that matches the username and password exists,
+        self.account is set to the account and the account is returned.
+        Otherwise, None is returned.
 
         :param username: Username entered by the user
         :param password: Password entered by the user
