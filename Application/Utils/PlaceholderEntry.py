9e1d9bfa61a96ecd8a339cc6de087bb4d1ae8ef4
Daniel Moffett <moffd234@gmail.com>
2025-05-31 05:03:18 -0400

    Wrote PlaceholderEntry.py methods

--- Application/Utils/PlaceholderEntry.py
+++ Application/Utils/PlaceholderEntry.py
@@ -11,3 +11,32 @@ class PlaceholderEntry(tkinter.Entry):
         # Bind functions on FocusIn and FocusOut
         self.bind("<FocusIn>", self.focus_in)
         self.bind("<FocusOut>", self.focus_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self) -> None:
+        """
+        Inserts placeholder text in the entry and sets foreground color.
+        :return: None
+        """
+        self.insert(0, self.placeholder)
+        self['fg'] = self.color
+
+    def focus_in(self, event) -> None:
+        """
+        Clears placeholder text in the entry if it is still showing on FocusIn.
+        :param event: Event automatically passed by tkinter
+        :return: None
+        """
+        if self['fg'] == self.color:
+            self.delete('0', 'end')
+            self['fg'] = self.default_fg_color
+
+    def focus_out(self, event=None) -> None:
+        """
+        Re-enters placeholder text in the entry if entry is empty on FocusOut.
+        :param event: Event automatically passed by tkinter
+        :return: None
+        """
+        if not self.get():
+            self.put_placeholder()
