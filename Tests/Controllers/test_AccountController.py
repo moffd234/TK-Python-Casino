1b1851ea6ff32081ab162dbf7b98b9a0863d236e
Daniel Moffett <moffd234@gmail.com>
2025-06-01 23:18:11 -0400

    Moved tests for is_password_valid and is_email_valid into test_AccountController.py

--- Tests/Controllers/test_AccountController.py
+++ Tests/Controllers/test_AccountController.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from Application.Controller.AccountController import AccountController
+from Application.Controller.AccountController import AccountController, is_password_valid
 from Application.Model.Accounts.AccountManager import AccountManager
 from Application.Model.Accounts.UserAccount import UserAccount
 from Tests.BaseTest import ACCOUNT_MANAGER_CLASS_PATH, BaseTest, TEST_QUESTIONS
@@ -28,3 +28,68 @@ class TestAccountController(BaseTest):
     def test_login_failure(self):
         actual: bool = self.account_controller.login("test_username", "InvalidPassword123!")
         self.assertFalse(actual)
+
+    def test_is_password_valid_true(self):
+        test_password: str = "validPassword123!"
+
+        self.assertTrue(is_password_valid(test_password))
+
+    def test_is_password_valid_exactly_8_chars(self):
+        test_password: str = "validP1!"
+
+        self.assertTrue(is_password_valid(test_password))
+
+    def test_is_password_valid_too_short(self):
+        test_password: str = "validPa"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_valid_no_uppercase(self):
+        test_password: str = "valid_password123!"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_valid_no_lowercase(self):
+        test_password: str = "VALID_PASSWORD123!"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_valid_only_letters(self):
+        test_password: str = "vAlIdPaSsWoRd"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_valid_no_number(self):
+        test_password: str = "validPassword!"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_only_number(self):
+        test_password: str = "12345678"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_valid_no_special(self):
+        test_password: str = "validPassword123"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_only_special(self):
+        test_password: str = "!@#$%^&*("
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_invalid_space_char(self):
+        test_password: str = "ValidPassword  123!"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_invalid_tab_char(self):
+        test_password: str = "ValidPassword\t123!"
+
+        self.assertFalse(is_password_valid(test_password))
+
+    def test_is_password_invalid_empty_string(self):
+        test_password: str = ""
+
+        self.assertFalse(is_password_valid(test_password))
