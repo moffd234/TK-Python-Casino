1d4912e548efe5887bec9a99b514819293b5c6eb
Daniel Moffett <moffd234@gmail.com>
2025-06-02 18:23:17 -0400

    Wrote get_account_from_email

--- Application/View/AccountValidationFrame.py
+++ Application/View/AccountValidationFrame.py
@@ -2,6 +2,7 @@ from tkinter import ttk
 from typing import TYPE_CHECKING
 
 from Application.Controller.MainWindow import MainWindow
+from Application.Model.Accounts.UserAccount import UserAccount
 from Application.Utils.PlaceholderEntry import PlaceholderEntry as pEntry
 from Application.View.BaseFrame import BaseFrame
 
@@ -15,10 +16,14 @@ class AccountValidationFrame(BaseFrame):
         self.controller: 'MainWindow' = controller
 
         self.email_entry: pEntry = pEntry(self, "Email")
+        self.answer_one: pEntry = pEntry(self, "Answer")
+        self.answer_two: pEntry = pEntry(self, "Answer")
 
         self.error_label = ttk.Label(parent)
+        self.answer_one_label = ttk.Label(parent)
+        self.answer_two_label = ttk.Label(parent)
 
-        self.validate_button: ttk.Button = ttk.Button(self, text="Validate")
+        self.validate_button: ttk.Button = ttk.Button(self, text="Validate", command=self.validate_email)
         self.back_button: ttk.Button = ttk.Button(self, text="Back")
 
         self.place()
@@ -28,5 +33,19 @@ class AccountValidationFrame(BaseFrame):
         self.validate_button.place(relx=0.4, rely=0.65, anchor="center")
         self.back_button.place(relx=0.6, rely=0.65, anchor="center")
 
-    def validate_email(self):
+    def get_account_from_email(self):
+        account: UserAccount = self.controller.account_controller.validate_email(self.email_entry.get())
+        if not account:
+            self.error_label.configure(text="Invalid email", foreground="red")
+            self.error_label.place(relx=0.5, rely=0.15, anchor="center")
+
+        else:
+            self.validate_button.configure(command=self.validate_security_answers)
+            self.place_security_questions()
+
+
+    def place_security_questions(self):
+        pass
+
+    def validate_security_answers(self):
         pass
