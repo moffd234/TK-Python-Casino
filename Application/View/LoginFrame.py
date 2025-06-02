df6b39de20ecc737c01705cf5a0e2ed4310000ca
Daniel Moffett <moffd234@gmail.com>
2025-06-02 17:57:51 -0400

    Added reset_password button

--- Application/View/LoginFrame.py
+++ Application/View/LoginFrame.py
@@ -17,6 +17,8 @@ class LoginFrame(BaseFrame):
         self.username_entry: pEntry = pEntry(self, placeholder="Username", width=50)
         self.password_entry: pEntry = pEntry(self, placeholder="Password", width=50)
 
+        self.reset_password_button: ttk.Button = ttk.Button(self, text="Reset Password",
+                                                            command=self.transition_to_validation)
         self.login_button: ttk.Button = ttk.Button(self, text="Login", command=self.login)
         self.back_button: ttk.Button = ttk.Button(self, text="Back", command=self.transition_back)
 
@@ -32,7 +34,7 @@ class LoginFrame(BaseFrame):
         account: bool = self.controller.account_controller.login(username, password)
 
         if account:
-           self.controller.render_frame(MainMenuFrame)
+            self.controller.render_frame(MainMenuFrame)
 
         else:
             self.error_label.place(relx=0.5, rely=0.5, anchor="center")
@@ -41,9 +43,14 @@ class LoginFrame(BaseFrame):
         from Application.View.EntryFrame import EntryFrame
         self.controller.render_frame(EntryFrame)
 
+    def transition_to_validation(self):
+        from Application.View.AccountValidationFrame import AccountValidationFrame
+        self.controller.render_frame(AccountValidationFrame)
+
     def place_elements(self):
         self.username_entry.place(relx=0.5, rely=0.35, anchor="center")
         self.password_entry.place(relx=0.5, rely=0.45, anchor="center")
 
         self.login_button.place(relx=0.45, rely=0.6, anchor="center")
         self.back_button.place(relx=0.55, rely=0.6, anchor="center")
+        self.reset_password_button.place(relx=0.5, rely=0.68, anchor="center")
