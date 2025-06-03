f4d6777c0d3b513c33b60732771bc8d958973759
Daniel Moffett <moffd234@gmail.com>
2025-06-03 17:03:11 -0400

    Created PasswordResetFrame

---
+++ Application/View/PasswordResetFrame.py
@@ -0,0 +1,13 @@
+from tkinter import ttk
+from typing import TYPE_CHECKING
+
+from Application.View.BaseFrame import BaseFrame
+
+
+if TYPE_CHECKING:
+    from Application.Controller.MainWindow import MainWindow
+
+
+class PasswordResetFrame(BaseFrame):
+    def __init__(self, parent: ttk.Frame, controller: 'MainWindow'):
+        BaseFrame.__init__(self, parent, controller)
