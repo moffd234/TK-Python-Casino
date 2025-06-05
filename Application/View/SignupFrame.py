b1d39639a05dc3871a3ffb36bcb9ddac8647e0e5
Daniel Moffett <moffd234@gmail.com>
2025-06-05 17:46:28 -0400

    Added UI elements

--- Application/View/SignupFrame.py
+++ Application/View/SignupFrame.py
@@ -1,6 +1,7 @@
 from tkinter import ttk
 from typing import TYPE_CHECKING
 
+from Application.Utils.PlaceholderEntry import PlaceholderEntry as pEntry
 from Application.View.BaseFrame import BaseFrame
 
 if TYPE_CHECKING:
@@ -11,3 +12,31 @@ class SignUpFrame(BaseFrame):
     def __init__(self, parent: ttk.Frame, controller: 'MainWindow'):
         super().__init__(parent, controller)
         self.controller: 'MainWindow' = controller
+
+        possible_questions: list[str] = [
+            "What is your favorite sports team?",
+            "What street did you grow up on?",
+            "What was the name of your first pet?",
+            "What is your mother’s maiden name?",
+            "What was the name of your elementary school?",
+            "In what city were you born?",
+            "What was the make of your first car?",
+            "What is your favorite movie?",
+            "What is your childhood best friend's first name?",
+            "What was the name of your first employer?",
+            "What is your favorite food?",
+            "What is the name of your favorite teacher?",
+            "Where did you go on your first vacation?",
+            "What is the middle name of your oldest sibling?",
+            "What was the name of your first stuffed animal?"
+        ]
+
+        self.username_entry: pEntry = pEntry(self, "Username", width=50)
+        self.password_entry: pEntry = pEntry(self, "Password", width=50)
+        self.email_entry: pEntry = pEntry(self, "Email", width=50)
+
+        self.security_question_one: ttk.Combobox = ttk.Combobox(self, values=possible_questions)
+        self.security_question_two: ttk.Combobox = ttk.Combobox(self, values=possible_questions)
+
+        self.reset_button: ttk.Button = ttk.Button(self, text="Signup", command="")
+        self.back_button: ttk.Button = ttk.Button(self, text="Back", command="")
