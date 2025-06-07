931c53740e7518f5ec46d0181f0cac13f503009f
Daniel Moffett <moffd234@gmail.com>
2025-06-07 16:32:29 -0400

    Wrote place_elements

--- Application/View/MainMenuFrame.py
+++ Application/View/MainMenuFrame.py
@@ -8,13 +8,19 @@ class MainMenuFrame(BaseFrame):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
         self.controller = controller
-        self.play_game_button: ttk.Button = ttk.Button(text="Play")
-        self.manage_account_button: ttk.Button = ttk.Button(text="Manage Account")
-        self.logout_button: ttk.Button = ttk.Button(text="Logout")
+        self.play_game_button: ttk.Button = ttk.Button(text="Play", width=15)
+        self.manage_account_button: ttk.Button = ttk.Button(text="Manage Account", width=15)
+        self.logout_button: ttk.Button = ttk.Button(text="Logout", width=15)
 
         style: ttk.Style = ttk.Style()
         style.configure(style="Title.TLabel", font=("TkDefaultFont", 25))
         self.title_label: ttk.Label = ttk.Label(text="Select an action", style="Title.TLabel")
 
-    def place_elements(self):
-        pass
+        self.place_elements()
+
+    def place_elements(self) -> None:
+        self.title_label.place(relx=0.5, rely=0.2, anchor="center")
+
+        self.play_game_button.place(relx=0.4, rely=0.5, anchor="center")
+        self.manage_account_button.place(relx=0.6, rely=0.5, anchor="center")
+        self.logout_button.place(relx=0.5, rely=0.6, anchor="center")
