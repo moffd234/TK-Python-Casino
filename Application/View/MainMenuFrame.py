5fec2e659c0f701526a45296bf3b7dbd1ec59fba
Daniel Moffett <moffd234@gmail.com>
2025-06-07 16:23:38 -0400

    Created UI elements

--- Application/View/MainMenuFrame.py
+++ Application/View/MainMenuFrame.py
@@ -1,3 +1,5 @@
+from tkinter import ttk
+
 from Application.View.BaseFrame import BaseFrame
 
 
@@ -6,6 +8,13 @@ class MainMenuFrame(BaseFrame):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
         self.controller = controller
+        self.play_game_button: ttk.Button = ttk.Button(text="Play")
+        self.manage_account_button: ttk.Button = ttk.Button(text="Manage Account")
+        self.logout_button: ttk.Button = ttk.Button(text="Logout")
+
+        style: ttk.Style = ttk.Style()
+        style.configure(style="Title.TLabel", font=("TkDefaultFont", 25))
+        self.title_label: ttk.Label = ttk.Label(text="Select an action", style="Title.TLabel")
 
     def place_elements(self):
         pass
