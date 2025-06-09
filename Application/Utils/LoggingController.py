f10995e1e3a5c7956504ebfd33ddc423cc572c8e
Daniel Moffett <moffd234@gmail.com>
2025-06-09 17:35:41 -0400

    Updated LoggingController.py to use handlers

--- Application/Utils/LoggingController.py
+++ Application/Utils/LoggingController.py
@@ -1,6 +1,22 @@
 import logging
 import os
+from logging import FileHandler, Formatter, basicConfig
 
-os.makedirs("logs", exist_ok=True)
-logging.basicConfig(filename="logs/casino.log", filemode="a",level=logging.INFO,
-                    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+PARENT_DIR = "logs"
+os.makedirs(PARENT_DIR, exist_ok=True)
+
+formatter: Formatter = Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+
+default_file_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "app.log"))
+default_file_handler.setFormatter(formatter)
+default_file_handler.setLevel(logging.DEBUG)
+
+basicConfig(level=logging.INFO, handlers=[default_file_handler]) # Set default logging to use file_handler
+
+auth_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "account.log"))
+auth_handler.setLevel(logging.DEBUG)
+auth_handler.setFormatter(formatter)
+
+db_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "database.log"))
+db_handler.setLevel(logging.DEBUG)
+db_handler.setFormatter(formatter)
