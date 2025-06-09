ebd0e575d35bf4c16d660c3830e1bdfb0a8c69f2
Daniel Moffett <moffd234@gmail.com>
2025-06-09 17:47:12 -0400

    Created auth_logger

--- Application/Utils/LoggingController.py
+++ Application/Utils/LoggingController.py
@@ -11,7 +11,7 @@ default_file_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "app.lo
 default_file_handler.setFormatter(formatter)
 default_file_handler.setLevel(logging.DEBUG)
 
-basicConfig(level=logging.INFO, handlers=[default_file_handler]) # Set default logging to use file_handler
+basicConfig(level=logging.INFO, handlers=[default_file_handler])  # Set default logging to use file_handler
 
 auth_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "account.log"))
 auth_handler.setLevel(logging.DEBUG)
@@ -20,3 +20,8 @@ auth_handler.setFormatter(formatter)
 db_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "database.log"))
 db_handler.setLevel(logging.DEBUG)
 db_handler.setFormatter(formatter)
+
+auth_logger = logging.getLogger("account.auth")
+auth_logger.addHandler(auth_handler)
+auth_logger.setLevel(logging.INFO)
+auth_logger.propagate = False
