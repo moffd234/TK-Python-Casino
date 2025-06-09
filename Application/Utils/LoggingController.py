0fd4f78235ea0d88188082c7bd4aa5e1efe24418
Daniel Moffett <moffd234@gmail.com>
2025-06-09 18:05:01 -0400

    Created setup_logging

--- Application/Utils/LoggingController.py
+++ Application/Utils/LoggingController.py
@@ -3,25 +3,34 @@ import os
 from logging import FileHandler, Formatter, basicConfig
 
 PARENT_DIR = "logs"
-os.makedirs(PARENT_DIR, exist_ok=True)
 
-formatter: Formatter = Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 
-default_file_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "app.log"))
-default_file_handler.setFormatter(formatter)
-default_file_handler.setLevel(logging.DEBUG)
+def setup_logging() -> None:
+    """
+    Configures application-wide logging.
 
-basicConfig(level=logging.INFO, handlers=[default_file_handler])  # Set default logging to use file_handler
+    This sets up log directories, formatters, handlers, and attaches handlers
+    to named loggers such as 'account.auth', 'database', and the default root logger.
+    """
+    os.makedirs(PARENT_DIR, exist_ok=True)
 
-auth_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "account.log"))
-auth_handler.setLevel(logging.DEBUG)
-auth_handler.setFormatter(formatter)
+    formatter: Formatter = Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 
-db_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "database.log"))
-db_handler.setLevel(logging.DEBUG)
-db_handler.setFormatter(formatter)
+    default_file_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "app.log"))
+    default_file_handler.setFormatter(formatter)
+    default_file_handler.setLevel(logging.DEBUG)
 
-auth_logger = logging.getLogger("account.auth")
-auth_logger.addHandler(auth_handler)
-auth_logger.setLevel(logging.INFO)
-auth_logger.propagate = False
+    basicConfig(level=logging.INFO, handlers=[default_file_handler])  # Set default logging to use file_handler
+
+    auth_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "account.log"))
+    auth_handler.setLevel(logging.DEBUG)
+    auth_handler.setFormatter(formatter)
+
+    db_handler: FileHandler = FileHandler(os.path.join(PARENT_DIR, "database.log"))
+    db_handler.setLevel(logging.DEBUG)
+    db_handler.setFormatter(formatter)
+
+    auth_logger = logging.getLogger("account.auth")
+    auth_logger.addHandler(auth_handler)
+    auth_logger.setLevel(logging.INFO)
+    auth_logger.propagate = False
