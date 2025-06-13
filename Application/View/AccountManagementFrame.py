cd6924ff15955b182a907460fa68c026e691939a
Daniel Moffett <moffd234@gmail.com>
2025-06-13 17:47:35 -0400

    Created AccountManagementFrame

---
+++ Application/View/AccountManagementFrame.py
@@ -0,0 +1,10 @@
+from Application.View.BaseFrame import BaseFrame
+
+
+class AccountManagementFrame(BaseFrame):
+
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+    def place_elements(self):
+        pass
