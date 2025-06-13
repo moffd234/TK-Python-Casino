25ddfa06c97a5ab60cefe10f7cb90ba7d49a8d19
Daniel Moffett <moffd234@gmail.com>
2025-06-13 17:44:18 -0400

    Fixed master of ttk elements

--- Application/View/MainMenuFrame.py
+++ Application/View/MainMenuFrame.py
@@ -8,14 +8,14 @@ class MainMenuFrame(BaseFrame):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
         self.controller = controller
-        self.play_game_button: ttk.Button = ttk.Button(text="Play", width=15, command=self.transition_to_game_screen)
-        self.manage_account_button: ttk.Button = ttk.Button(text="Manage Account", width=15,
+        self.play_game_button: ttk.Button = ttk.Button(self, text="Play", width=15, command=self.transition_to_game_screen)
+        self.manage_account_button: ttk.Button = ttk.Button(self, text="Manage Account", width=15,
                                                             command=self.transition_to_account_management)
-        self.logout_button: ttk.Button = ttk.Button(text="Logout", width=15, command=self.logout_user)
+        self.logout_button: ttk.Button = ttk.Button(self, text="Logout", width=15, command=self.logout_user)
 
         style: ttk.Style = ttk.Style()
         style.configure(style="Title.TLabel", font=("TkDefaultFont", 25))
-        self.title_label: ttk.Label = ttk.Label(text="Select an action", style="Title.TLabel")
+        self.title_label: ttk.Label = ttk.Label(self, text="Select an action", style="Title.TLabel")
 
         self.place_elements()
 
@@ -36,4 +36,4 @@ class MainMenuFrame(BaseFrame):
         from Application.View.EntryFrame import EntryFrame
 
         self.controller.account_controller.account = None
-        self.controller.render_frame(EntryFrame)
+        self.controller.render_frame(EntryFrame, show_menu=False)
