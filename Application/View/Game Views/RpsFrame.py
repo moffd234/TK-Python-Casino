cd6b987dd16b5f0c11bfa08b6bb403dbf5295a3d
Daniel Moffett <moffd234@gmail.com>
2025-06-15 20:30:41 -0400

    Created RpsFrame

---
+++ Application/View/Game Views/RpsFrame.py
@@ -0,0 +1,9 @@
+from Application.View.BaseFrame import BaseFrame
+
+
+class RpsFrame(BaseFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+    def place_elements(self):
+        pass
