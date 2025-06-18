f8775d430f7741758a038001e6bc761cea12c0fc
Daniel Moffett <moffd234@gmail.com>
2025-06-18 16:31:44 -0400

    Removed print_welcome_message method from CoinFlip.py

--- Application/Model/Games/CoinFlip/CoinFlip.py
+++ Application/Model/Games/CoinFlip/CoinFlip.py
@@ -17,21 +17,6 @@ class CoinFlip(Game):
         super().__init__()
         self.console.color = ANSI_COLORS.BLUE.value
 
-    def print_welcome_message(self) -> None:
-        self.console.print_colored(r"""
-        
-        Yb        dP 888888 88      dP""b8  dP"Yb  8b    d8 888888     888888  dP"Yb       dP""b8  dP"Yb  88 88b 88     888888 88     88 88""Yb 
-         Yb  db  dP  88__   88     dP   `" dP   Yb 88b  d88 88__         88   dP   Yb     dP   `" dP   Yb 88 88Yb88     88__   88     88 88__dP 
-          YbdPYbdP   88""   88  .o Yb      Yb   dP 88YbdP88 88""         88   Yb   dP     Yb      Yb   dP 88 88 Y88     88""   88  .o 88 88
-           YP  YP    888888 88ood8  YboodP  YbodP  88 YY 88 888888       88    YbodP       YboodP  YbodP  88 88  Y8     88     88ood8 88 88     
-        
-        rules:
-             1. Enter a guess of either heads or tails
-             2. A coin will be flipped
-             3. If you guess correctly you will win 1.25x your wager
-        """)
-
-
     def get_guess(self) -> str:
         guess: str = self.console.get_string_input("Enter your guess: (heads or tails)")
 
@@ -43,7 +28,6 @@ class CoinFlip(Game):
 
     def handle_outcome(self, guess: str, flip: str, wager: float):
         if guess == flip:
-            self.manager.add_and_save_account(self.player, wager * 1.25)
             return f"You Won! The coin was {flip}"
 
         else:
