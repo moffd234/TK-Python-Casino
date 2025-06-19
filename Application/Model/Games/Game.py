30a1f9bbd089f4fac1c5b2507b1fdce61f56a968
Daniel Moffett <moffd234@gmail.com>
2025-06-19 16:03:15 -0400

    Removed abstract methods from Game.py

--- Application/Model/Games/Game.py
+++ Application/Model/Games/Game.py
@@ -6,14 +6,3 @@ from Application.Utils.IOConsole import IOConsole
 class Game(ABC):
     def __init__(self):
         self.console = IOConsole()
-
-    @abstractmethod
-    def print_welcome_message(self) -> None:
-        pass
-
-    @abstractmethod
-    def run(self):
-        pass
-
-    def get_continue_input(self) -> bool:
-        return self.console.get_boolean_input("Would you like to keep playing?")
