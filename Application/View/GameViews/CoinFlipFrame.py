25a39093921a5053f1b89bacc477feb4af64406a
Daniel Moffett <moffd234@gmail.com>
2025-06-25 15:25:11 -0400

    Fixed images on buttons

--- Application/View/GameViews/CoinFlipFrame.py
+++ Application/View/GameViews/CoinFlipFrame.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import ttk, PhotoImage
 from PIL import Image, ImageTk
 from PIL.Image import Image as PILImage
@@ -10,15 +11,21 @@ class CoinFlipFrame(BaseFrame):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
 
-        img: PILImage = Image.open("../../Assets/cs_tails.png").resize((100, 100), Image.Resampling.LANCZOS)
-        tails_image: PhotoImage = ImageTk.PhotoImage(img)
-        self.tails_button: ttk.Button = ttk.Button(self, image=tails_image, command="")
+        ASSETS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../Assets"))
+        tails_image_path = os.path.join(ASSETS_PATH, "cs_tails.png")
+        heads_image_path = os.path.join(ASSETS_PATH, "cs_heads.png")
 
-        img = Image.open("../../Assets/cs_heads.png").resize((100, 100), Image.Resampling.LANCZOS)
-        heads_image: PhotoImage = ImageTk.PhotoImage(img)
-        self.tails_button: ttk.Button = ttk.Button(self, image=heads_image, command="")
+        img: PILImage = Image.open(tails_image_path).resize((100, 100), Image.Resampling.LANCZOS)
+        self.tails_image: PhotoImage = ImageTk.PhotoImage(img)
+        self.tails_button: ttk.Button = ttk.Button(self, image=self.tails_image, command="")
+
+        img = Image.open(heads_image_path).resize((100, 100), Image.Resampling.LANCZOS)
+        self.heads_image: PhotoImage = ImageTk.PhotoImage(img)
+        self.heads_button: ttk.Button = ttk.Button(self, image=self.heads_image, command="")
 
         self.prompt_label: ttk.Label = ttk.Label(self, text="Choose A Coin Side", font=("Helvetica", 16, "bold"))
+
+        self.place_elements()
 
     def place_elements(self):
         pass
