5af375d47507ccc4da8f2c968c71008426d267eb
Daniel Moffett <moffd234@gmail.com>
2025-06-26 19:57:46 -0400

    Changed validate_float to have a default min_value of 1.00

--- Application/Utils/TypeValidation.py
+++ Application/Utils/TypeValidation.py
@@ -1,4 +1,4 @@
-def validate_float(user_input: str, min_value=None):
+def validate_float(user_input: str, min_value=1.00):
     try:
         float_val: float = float(user_input)
         if min_value:
