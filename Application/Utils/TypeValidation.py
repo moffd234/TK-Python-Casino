dcc340bf5d9259e8f9dabb5883e703f046728e67
Daniel Moffett <moffd234@gmail.com>
2025-06-26 20:00:03 -0400

    Added docstring for validate_float

--- Application/Utils/TypeValidation.py
+++ Application/Utils/TypeValidation.py
@@ -1,4 +1,12 @@
 def validate_float(user_input: str, min_value=1.00):
+    """
+    Validates that the given input string represents a float greater than a minimum value.
+
+    :param user_input: String inputted by user
+    :param min_value: The minimum acceptable float value. Defaults to 1.00.
+    :return: True if the input is a valid float greater than min_value, or if no minimum is specified. False if the
+             input is not a valid float or does not meet the minimum requirement.
+    """
     try:
         float_val: float = float(user_input)
         if min_value:
