e8af9a8c0b2bf352a3a24ad5b1f072a032c582e4
Daniel Moffett <moffd234@gmail.com>
2025-06-27 19:28:29 -0400

    Moved flip to handle_outcome

--- Application/Controller/Games/CoinFlipController.py
+++ Application/Controller/Games/CoinFlipController.py
@@ -5,11 +5,11 @@ from Application.Model.Games.CoinFlip.CoinFlip import handle_heads_tails
 class CoinFlipController:
 
     def __init__(self, account_controller: AccountController):
-        self.flip: str = handle_heads_tails()
         self.account_controller = account_controller
 
     def handle_outcome(self, guess: str, wager: float) -> bool:
-        if self.flip == guess:
+        flip: str = handle_heads_tails()
+        if flip == guess:
             self.account_controller.add_winnings(wager * 1.25)
             return True
 
