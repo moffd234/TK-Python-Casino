0044cbb140fde220c9236ca459910128e82e309d
Daniel Moffett <moffd234@gmail.com>
2025-06-27 19:42:28 -0400

    Changed CoinFlipController.handle_outcome to use GameOutcome

--- Application/Controller/Games/CoinFlipController.py
+++ Application/Controller/Games/CoinFlipController.py
@@ -1,5 +1,6 @@
 from Application.Controller.AccountController import AccountController
 from Application.Model.Games.CoinFlip.CoinFlip import handle_heads_tails
+from Application.Model.Games.GameOutcome import GameOutcome
 
 
 class CoinFlipController:
@@ -7,15 +8,15 @@ class CoinFlipController:
     def __init__(self, account_controller: AccountController):
         self.account_controller = account_controller
 
-    def handle_outcome(self, guess: str, wager: float) -> int:
+    def handle_outcome(self, guess: str, wager: float) -> GameOutcome:
         flip: str = handle_heads_tails()
         successful_withdraw: bool = self.account_controller.subtract_losses(wager)
 
         if successful_withdraw:
             if flip == guess:
                 self.account_controller.add_winnings(wager * 1.25)
-                return 0
+                return GameOutcome.WIN
 
-            return 1
+            return GameOutcome.LOSS
 
-        return 2
+        return GameOutcome.WITHDRAW_ERROR
