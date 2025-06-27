3f36682584fcc24ca20cffb34d0bc52b323ef7a9
Daniel Moffett <moffd234@gmail.com>
2025-06-26 20:25:02 -0400

    Fixed validate_float not allow 1.0

--- Application/Utils/TypeValidation.py
+++ Application/Utils/TypeValidation.py
@@ -13,7 +13,7 @@ def validate_float(user_input: str, min_value=1.00):
     try:
         float_val: float = float(user_input)
         if min_value:
-            return float_val > min_value
+            return float_val >= min_value
         return True
 
     except ValueError:
