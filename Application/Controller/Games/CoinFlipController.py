08e9ded027dcd634279fe05d92be51faac3cc30a
Daniel Moffett <moffd234@gmail.com>
2025-06-28 20:21:52 -0400

    Wrote docstring for handle_outcome

--- Application/Controller/Games/CoinFlipController.py
+++ Application/Controller/Games/CoinFlipController.py
@@ -9,6 +9,14 @@ class CoinFlipController:
         self.account_controller = account_controller
 
     def handle_outcome(self, guess: str, wager: float) -> GameOutcome:
+        """
+        Handles the logic of a coin flip game, including checking the user's guess,
+        updating account balance based on the result, and returning the outcome.
+
+        :param guess: The user's guess for the coin flip ("heads" or "tails").
+        :param wager: The amount of money the user is wagering.
+        :return: GameOutcome indicating WIN, LOSS, or WITHDRAW_ERROR.
+        """
         flip: str = handle_heads_tails()
         successful_withdraw: bool = self.account_controller.subtract_losses(wager)
 
