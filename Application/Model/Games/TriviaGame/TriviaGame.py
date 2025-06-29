9447f327a5249b8ca7b4157239be94fee8dc465c
Daniel Moffett <moffd234@gmail.com>
2025-06-28 20:40:49 -0400

    Wrote create_questions

--- Application/Model/Games/TriviaGame/TriviaGame.py
+++ Application/Model/Games/TriviaGame/TriviaGame.py
@@ -1,5 +1,19 @@
+from html import unescape
+
+from Application.Model.Games.TriviaGame.Question import Question
+
+
+def create_questions(q_response: dict) -> list[Question]:
+    questions_list: list[Question] = []
+    for question in q_response["results"]:
+        questions_list.append(Question(question=unescape(question["question"]),
+                                       answer=unescape(question["correct_answer"]),
+                                       wrong_answers=[unescape(answer) for answer in question["incorrect_answers"]]
+                                       ))
+    return questions_list
+
+
 class TriviaGame:
 
     def __init__(self):
         pass
-
