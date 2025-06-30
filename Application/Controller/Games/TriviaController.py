40e86761adbc5927634e396f0780a38179eca54d
Daniel Moffett <moffd234@gmail.com>
2025-06-30 16:59:48 -0400

    Wrote start_game

--- Application/Controller/Games/TriviaController.py
+++ Application/Controller/Games/TriviaController.py
@@ -1,8 +1,20 @@
 from Application.Controller.AccountController import AccountController
+from Application.Model.Games.TriviaGame.Category import Category
+from Application.Model.Games.TriviaGame.TriviaGame import TriviaGame
 
 
 class TriviaController:
 
     def __init__(self, account_controller: AccountController):
         self.account_controller: AccountController = account_controller
+        self.game: TriviaGame | None = None
 
+    def start_game(self, q_type: str, diff: str, cat: Category) -> None:
+        """
+        Initializes a new TriviaGame instance with the given parameters.
+
+        :param q_type: Question type ("boolean" or "multiple").
+        :param diff: Difficulty level ("easy", "medium", "hard").
+        :param cat: Selected Category instance.
+        """
+        self.game = TriviaGame(q_type, diff, cat)
